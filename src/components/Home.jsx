de3efc6c04b8270f25b38fe2db9aeeba17ae8be5
Aravind P <araviindp@gmail.com>
2024-12-15 22:28:05 +0530

    smooth scroll add in home page buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import { CgPhone } from "react-icons/cg";
 import { BsPerson } from "react-icons/bs";
 import { useSelector } from "react-redux";
 import { motion } from "framer-motion";
+import { scroller } from 'react-scroll';
 
 const Home = () => {
   const { darkMode } = useSelector((state) => state.appSlice);
@@ -19,11 +20,18 @@ const Home = () => {
     visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
   };
 
+  const handleClick = (id) => {
+    scroller.scrollTo(id, {
+      duration: 500,
+      smooth: true,
+      offset: -70, // Adjust for fixed navbar height
+    });
+  }
+
   return (
     <motion.div
-      className={`flex flex-col items-center justify-center min-h-[100vh] px-4 py-8 ${
-        darkMode ? "bg-background-dark" : "bg-background-light"
-      } w-full overflow-x-hidden mt-10`}
+      className={`flex flex-col items-center justify-center min-h-[100vh] px-4 py-8 ${darkMode ? "bg-background-dark" : "bg-background-light"
+        } w-full overflow-x-hidden mt-10`}
       initial="hidden"
       animate="visible"
       variants={containerVariants}
@@ -34,17 +42,15 @@ const Home = () => {
         variants={itemVariants}
       >
         <motion.h1
-          className={`text-3xl md:text-4xl lg:text-5xl font-bold ${
-            darkMode ? "text-text-dark2" : "text-text-light2"
-          }`}
+          className={`text-3xl md:text-4xl lg:text-5xl font-bold ${darkMode ? "text-text-dark2" : "text-text-light2"
+            }`}
           variants={itemVariants}
         >
           Hello there!
         </motion.h1>
         <motion.h1
-          className={`text-3xl md:text-4xl lg:text-5xl font-bold ${
-            darkMode ? "text-text-dark" : "text-text-light"
-          }`}
+          className={`text-3xl md:text-4xl lg:text-5xl font-bold ${darkMode ? "text-text-dark" : "text-text-light"
+            }`}
           variants={itemVariants}
         >
           I'M <b>Aravind P</b>
@@ -59,26 +65,24 @@ const Home = () => {
           className="flex flex-col space-y-4 md:space-y-6 lg:space-y-8"
           variants={itemVariants}
         >
-          <a href="#about" className="w-full">
+          <a className="w-full" onClick={()=>handleClick("about")}>
             <motion.div
-              className={`flex items-center justify-center px-6 py-3 ${
-                darkMode
+              className={`flex items-center justify-center px-6 py-3 ${darkMode
                   ? "bg-button-dark text-text-dark hover:bg-button-dark-hover"
                   : "bg-button-light text-text-light hover:bg-button-light-hover"
-              } rounded-md shadow-md transition duration-300 cursor-pointer`}
+                } rounded-md shadow-md transition duration-300 cursor-pointer`}
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
             >
               About Me <BsPerson className="ml-2" />
             </motion.div>
           </a>
-          <a href="#contact" className="w-full">
+          <a className="w-full" onClick={()=>handleClick("contact")}>
             <motion.div
-              className={`flex items-center justify-center px-6 py-3 ${
-                darkMode
+              className={`flex items-center justify-center px-6 py-3 ${darkMode
                   ? "bg-button-dark2 text-text-dark hover:bg-button-dark2-hover"
                   : "bg-button-light2 text-text-light hover:bg-button-light2-hover"
-              } rounded-md shadow-md transition duration-300 cursor-pointer`}
+                } rounded-md shadow-md transition duration-300 cursor-pointer`}
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
             >
